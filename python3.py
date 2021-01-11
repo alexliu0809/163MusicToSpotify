b661193dba52d02baff69588f8b162d0b25a970e
Alex <alexliu0809@hotmail.com>
2021-01-11 14:30:38 -0800

    add third-party

--- python3.py
+++ python3.py
@@ -1,5 +1,6 @@
 import json
 import sys, io
+import cloudmusic
 import urllib3
 
 #Export Your NetEase Cloud Music Playlist To Sporify (From NetEase Cloud Music To Sportify!!!)
@@ -10,32 +11,28 @@ import urllib3
 
 
 #把下面这个ID替换成你要导入的歌单的ID
-playlistId = 120932413 #### Put Your 9 digits playlist id here
+playlistId = 5374691276 #### Put Your 9 digits playlist id here
 
 #The Result Will Be Saved To The Current Directory With Name: "YourPlayList.txt"
 #导出的歌单保存在当前目录，文件名：歌单名（可能是中文）.txt
 
 ############ Don't Touch Anything Below ############
 ############ 下面的别动 ############ 
+playlist = cloudmusic.getPlaylist(playlistId)
 
-url = "http://music.163.com/api/playlist/detail?id=" + str(playlistId) + "&updateTime=-1" #Request Url From Netease
+output = []
+for music in playlist:
+	output.append(music.name + "-" + ",".join(music.artist) + "\n")
 
-http = urllib3.PoolManager()
-
-response = http.request('GET', url)
-
-data = json.loads(response.data)
-tracks = data["result"]["tracks"]
-
-# Your code where you can use urlopen
-tracks = data["result"]["tracks"]
-output = ""
-for track in tracks:
-	#print(len(tracks))
-	trackName = track["name"]
-	artist = track["artists"][0]["name"]
-	output += trackName + ' - ' + artist + '\n'
-playlistName = data["result"]["name"]
+# # Your code where you can use urlopen
+# tracks = data["result"]["tracks"]
+# output = ""
+# for track in tracks:
+# 	#print(len(tracks))
+# 	trackName = track["name"]
+# 	artist = track["artists"][0]["name"]
+# 	output += trackName + ' - ' + artist + '\n'
+# playlistName = data["result"]["name"]
 
 with open(playlistName+'.txt', 'w',encoding='utf-8') as file:
 	file.write(output)
